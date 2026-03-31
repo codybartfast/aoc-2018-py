bebbd76d937ffe6a2f5fea586b6622e70f9d2143
codybartfast <john@codybartfast.com>
2026-03-31 16:54:15 +0100

    ☕

--- day03.py
+++ day03.py
@@ -1,5 +1,24 @@
-from array import array
+#  2018 Day 3
+#  ==========
+#
+#  Part 1: 115242
+#  Part 2: 1046
+#
+#  Timings
+#  --------------------------------------
+#      Parse:     0.001387s  (1.387 ms)
+#     Part 1:     0.016001s  (16.00 ms)
+#     Part 2:     0.015591s  (15.59 ms)
+#    Elapsed:     0.033024s  (33.02 ms)
+#  --------------------------------------
+#
+#     Date:  March 2026
+#  Machine:  MacBook M4
+#   Python:  3.14.3
+
+
 import re
+
 
 def parse(text):
     re_digits = re.compile(r"-?\d+")
@@ -7,15 +26,14 @@ def parse(text):
     def parse_line(line):
         [id, x, y, w, h] = list(map(int, re_digits.findall(line)))
         return id, (x, y), (w, h)
-    
-    lines = text.splitlines()
-    return [parse_line(line) for line in lines]
+
+    return [parse_line(line) for line in text.splitlines()]
 
 
-def part1(data, args, p1_state):
+def part1(panels, args, p1_state):
     width = height = 1000
-    cloth = array("B", [0] * width * height)
-    for _, (x, y), (w, h) in data:
+    cloth = [0] * width * height
+    for _, (x, y), (w, h) in panels:
         for row in range(y, y + h):
             start = row * width + x
             for idx in range(start, start + w):
@@ -23,11 +41,11 @@ def part1(data, args, p1_state):
     return sum(1 for square in cloth if square > 1)
 
 
-def part2(data, args, p1_state):
+def part2(panels, args, p1_state):
     width = height = 1000
-    cloth = array("H", [0] * width * height)
-    candidates = set((panel[0] for panel in data))
-    for id, (x, y), (w, h) in data:
+    cloth = [0] * width * height
+    candidates = set((panel[0] for panel in panels))
+    for id, (x, y), (w, h) in panels:
         for row in range(y, y + h):
             start = row * width + x
             for idx in range(start, start + w):
@@ -45,7 +63,7 @@ def part2(data, args, p1_state):
 
 def collect_stars(text=None, filepath=None, extra_args=None):
     import workshop as ws
-    
+
     if not text and filepath:
         text = open(filepath).read().strip()
     ws.get_cracking(text, parse, part1, part2, extra_args)
