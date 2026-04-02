3a55bdf7dde184ab0dc28be9a919358152a8bb06
codybartfast <john@codybartfast.com>
2026-04-02 09:35:09 +0100

    day05 part2

--- day05.py
+++ day05.py
@@ -1,23 +1,30 @@
 def parse(text):
     return text.encode()
 
-def part1(polymer, args, p1_state):
-    print(f"\n{polymer}\n")
+
+def react_without(polymer, strip=0):
     reacted = []
     idx = 0
 
     while idx < len(polymer):
-        reacted.append(polymer[idx])
+        unit = polymer[idx]
+        if not (unit & 0b1011111) == strip:
+            reacted.append(unit)
         idx += 1
-        while(reacted and idx < len(polymer) and reacted[-1] ^ polymer[idx] == 32):
+        while reacted and idx < len(polymer) and reacted[-1] ^ polymer[idx] == 32:
             reacted.pop()
             idx += 1
 
-    return len(reacted)
+    return reacted
 
 
-def part2(data, args, p1_state):
-    return "ans2"
+def part1(polymer, args, p1_state):
+    return len(react_without(polymer))
+
+
+def part2(polymer, args, p1_state):
+    types = set(polymer.upper())
+    return min(len(react_without(polymer, type)) for type in types)
 
 
 # Runner
@@ -26,7 +33,7 @@ def part2(data, args, p1_state):
 
 def collect_stars(text=None, filepath=None, extra_args=None):
     import workshop as ws
-    
+
     if not text and filepath:
         text = open(filepath).read().strip()
     ws.get_cracking(text, parse, part1, part2, extra_args)
