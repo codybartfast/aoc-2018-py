e7c013591be9963ef557202b3fecf33d4a11ea7d
codybartfast <john@codybartfast.com>
2026-04-03 15:09:06 +0100

    day07 part2

--- day07.py
+++ day07.py
@@ -1,5 +1,6 @@
 import re
 
+
 def parse(text):
     re_digits = re.compile(r"-?\d+")
 
@@ -11,27 +12,61 @@ def parse(text):
     return [parse_line(line) for line in lines]
 
 
-def part1(data, args, p1_state):
-    data.sort(key=lambda kvp: kvp[1])
-    all_steps = list(sorted(set(step for instr in data for step in instr)))
+def relations(instructions):
+    instructions.sort(key=lambda kvp: kvp[1])
+    all_steps = list(sorted(set(step for instr in instructions for step in instr)))
     supports = {step: [] for step in all_steps}
     requires = {step: [] for step in all_steps}
-    for a,b in data:
+    for a, b in instructions:
         supports[a].append(b)
         requires[b].append(a)
+    return supports, requires
+
+
+def part1(instructions, args, p1_state):
+    supports, requires = relations(instructions)
 
     steps = []
-    for _ in range(len(all_steps)):
+    for _ in range(len(supports)):
         step = [key for key, val in requires.items() if not val][0]
         requires.pop(step)
-        for dependency in supports[step]:
-            requires[dependency].remove(step)
+        for dependent in supports[step]:
+            requires[dependent].remove(step)
         steps.append(step)
     return "".join(steps)
 
 
-def part2(data, args, p1_state):
-    return "ans2"
+def part2(instructions, args, p1_state):
+    time_const = 60
+    free_elves = 5
+
+    supports, requires = relations(instructions)
+
+    n_steps = len(supports)
+    in_progress = {}
+    time = 0
+    steps = []
+
+    while len(steps) < n_steps:
+        while free_elves and (
+            available := [step for step, reqs in requires.items() if not reqs]
+        ):
+            step = available[0]
+            requires.pop(step)
+            free_elves -= 1
+            finish_time = time + time_const + ord(step) - 0x40
+            in_progress.setdefault(finish_time, []).append(step)
+
+        time = min(in_progress)
+        completed = in_progress[time]
+        in_progress.pop(time)
+        for step in completed:
+            steps.append(step)
+            free_elves += 1
+            for dependent in supports[step]:
+                requires[dependent].remove(step)
+
+    return time
 
 
 # Runner
@@ -40,7 +75,7 @@ def part2(data, args, p1_state):
 
 def collect_stars(text=None, filepath=None, extra_args=None):
     import workshop as ws
-    
+
     if not text and filepath:
         text = open(filepath).read().strip()
     ws.get_cracking(text, parse, part1, part2, extra_args)
