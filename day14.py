1e2c39697d22c3133ae7e2a6e3ba22817c856127
codybartfast <john@codybartfast.com>
2026-04-07 20:39:32 +0100

    ☕

--- day14.py
+++ day14.py
@@ -1,3 +1,25 @@
+#  2018 Day 14
+#  ===========
+#
+#  Part 1: 6126491027
+#  Part 2: 20191615
+#
+#  Timings
+#  --------------------------------------
+#      Parse:     0.000000s  (0.291 µs)
+#     Part 1:     0.021608s  (21.61 ms)
+#     Part 2:     1.471188s  (1.471 s)
+#    Elapsed:     1.492864s  (1.493 s)
+#  --------------------------------------
+#
+#     Date:  April 2026
+#  Machine:  MacBook M4
+#   Python:  3.14.3
+
+
+# Since relatively straightforward did lots of micro optimisations so not pretty
+
+
 def parse(text):
     return text
 
@@ -25,23 +47,44 @@ def part1(text, args, p1_state):
 def part2(text, args, p1_state):
 
     target = [int(c) for c in text]
+    [t1, t2, t3, t4, t5, t6] = target
+    assert t6 == 1  # almost certainly always the case.
+    assert t5 != 1  # if true don't need to check second of two appends
     t_len = len(target)
 
     recipes = [3, 7]
+    rep_len = 2
     elf1 = 0
     elf2 = 1
 
     while True:
-        sum = (r1 := recipes[elf1]) + (r2 := recipes[elf2])
+        sum = (rcp1 := recipes[elf1]) + (rcp2 := recipes[elf2])
         if sum >= 10:
             recipes.append(1)
-            if recipes[-t_len:] == target:
+            if (
+                recipes[-2] == t5
+                and recipes[-3] == t4
+                and recipes[-4] == t3
+                and recipes[-5] == t2
+                and recipes[-6] == t1
+            ):
                 break
-        recipes.append(sum % 10)
-        if recipes[-t_len:] == target:
-            break
-        elf1 = (elf1 + 1 + r1) % len(recipes)
-        elf2 = (elf2 + 1 + r2) % len(recipes)
+            recipes.append(sum % 10)
+            rep_len += 2
+        else:
+            recipes.append(d := sum % 10)
+            rep_len += 1
+            if (
+                d == 1
+                and recipes[-2] == t5
+                and recipes[-3] == t4
+                and recipes[-4] == t3
+                and recipes[-5] == t2
+                and recipes[-6] == t1
+            ):
+                break
+        elf1 = (elf1 + 1 + rcp1) % rep_len
+        elf2 = (elf2 + 1 + rcp2) % rep_len
 
     return len(recipes) - t_len
 
