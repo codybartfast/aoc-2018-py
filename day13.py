81de6768e0216157a6ce2ada8968d35b6ccd333e
codybartfast <john@codybartfast.com>
2026-04-07 12:30:39 +0100

    ☕

--- day13.py
+++ day13.py
@@ -1,3 +1,25 @@
+#  2018 Day 13
+#  ===========
+#
+#  Part 1: 43,111
+#  Part 2: 44,56
+#
+#  Timings
+#  --------------------------------------
+#      Parse:     0.000868s  (868.0 µs)
+#     Part 1:     0.005971s  (5.971 ms)
+#     Part 2:     0.000000s  (0.334 µs)
+#    Elapsed:     0.006872s  (6.872 ms)
+#  --------------------------------------
+#
+#     Date:  April 2026
+#  Machine:  MacBook M4
+#   Python:  3.14.3
+
+
+PSTN = 0
+
+
 def parse(text):
 
     lines = text.splitlines()
@@ -26,78 +48,57 @@ def parse(text):
     return width, "".join(track), carts
 
 
-def advance(DIRS, track, cart, _width):
-    pstn, dir, choice = cart
-
-    match track[pstn]:
-        case "|" | "-":
-            pass
-        case "/":
-            dir = (dir + (1 if dir in [0, 2] else -1)) % 4
-        case "\\":
-            dir = (dir + (1 if dir in [1, 3] else -1)) % 4
-        case "+":
-            dir = (dir + choice - 1) % 4
-            choice = (choice + 1) % 3
-        case _:
-            assert False, (pstn % 108, pstn // 108, track[pstn])
-    pstn += DIRS[dir]
-    return pstn, dir, choice
-
-
 def part1(survey, args, p1_state):
-    # print(f"\n{survey}\n")
     width, track, carts = survey
     DIRS = [-width, 1, width, -1]
 
-    pstns = [cart[0] for cart in carts]
-    collision = None
-    while not collision:
+    pstns = set([cart[0] for cart in carts])
+    first_collision = None
+    wreck = None
+    while len(carts) > 1:
         next_carts = []
-        for cart in carts:
-            pstn = cart[0]
-            cart = advance(DIRS, track, cart, width)
+
+        for pstn, dir, choice in carts:
+            if pstn == wreck:
+                wreck = None
+                continue
             pstns.remove(pstn)
-            pstn = cart[0]
-            if pstn in pstns:
-                collision = pstn
-                break
-            pstns.append(pstn)
-            next_carts.append(cart)
+
+            match track[pstn]:
+                case "|" | "-":
+                    pass
+                case "/":
+                    dir = (dir + (1 if dir in [0, 2] else -1)) % 4
+                case "\\":
+                    dir = (dir + (1 if dir in [1, 3] else -1)) % 4
+                case "+":
+                    dir = (dir + choice - 1) % 4
+                    choice = (choice + 1) % 3
+                case _:
+                    assert False
+            pstn += DIRS[dir]
+
+            if pstn not in pstns:
+                pstns.add(pstn)
+                next_carts.append((pstn, dir, choice))
+            else:
+                if not first_collision:
+                    first_collision = pstn
+                pstns.remove(pstn)
+                n_next = len(next_carts)
+                next_carts = [cart for cart in next_carts if cart[PSTN] != pstn]
+                if len(next_carts) == n_next:
+                    wreck = pstn
         carts = next_carts
 
-    return f"{collision % width},{collision // width}"
+    last_cart = carts[0][PSTN]
+    p1_state.value = f"{last_cart % width},{last_cart // width}"
+
+    return f"{first_collision % width},{first_collision // width}"
 
 
 def part2(survey, args, p1_state):
-    width, track, carts = survey
-    DIRS = [-width, 1, width, -1]
-
-    pstns = [cart[0] for cart in carts]
-    crashed = None
-    while len(carts) > 1:
-        next_carts = []
-        for cart in carts:
-            if cart == crashed:
-                crashed = None
-                continue
-            pstn = cart[0]
-            cart = advance(DIRS, track, cart, width)
-            pstns.remove(pstn)
-            pstn = cart[0]
-            if pstn in pstns:
-                pstns.remove(pstn)
-                n_next = len(next_carts)
-                next_carts = [cart for cart in next_carts if cart[0] != pstn]
-                if len(next_carts) == n_next:
-                    crashed = [cart for cart in carts if cart[0] == pstn][0]
-            else:
-                pstns.append(pstn)
-                next_carts.append(cart)
-        carts = next_carts
-
-    last_collision = carts[0][0]
-    return f"{last_collision % width},{last_collision // width}"
+    return p1_state.value
 
 
 # Runner
