bc474c86c9583e5f01ae046506b1dcec3cf935e6
codybartfast <john@codybartfast.com>
2026-04-21 15:24:00 +0100

    ☕

--- day19.py
+++ day19.py
@@ -1,3 +1,22 @@
+#  2018 Day 19
+#  ===========
+#
+#  Part 1: 2223
+#  Part 2: 24117312
+#
+#  Timings
+#  --------------------------------------
+#      Parse:     0.000012s  (11.96 µs)
+#     Part 1:     0.000047s  (47.04 µs)
+#     Part 2:     0.002634s  (2.634 ms)
+#    Elapsed:     0.002728s  (2.728 ms)
+#  --------------------------------------
+#
+#     Date:  April 2026
+#  Machine:  MacBook M4
+#   Python:  3.14.3
+
+
 def parse(text):
 
     def parse_line(line):
@@ -17,13 +36,9 @@ def boot(reg_zero=0):
 def run(pcr, prog, regs):
     pc = 0
 
-    # blah = 0 if regs[0] else 1001
-
     while 0 <= pc < len(prog):
         regs[pcr] = pc
         opname, arg1, arg2, dest = prog[pc]
-
-        # print(pc, prog[pc], regs)
 
         match opname:
             case "addr":
@@ -51,7 +66,6 @@ def run(pcr, prog, regs):
             case "gtri":
                 regs[dest] = regs[arg1] > arg2
             case "gtrr":
-                # print(pc, regs[arg1], regs[arg2], regs[arg1] > regs[arg2])
                 regs[dest] = regs[arg1] > regs[arg2]
             case "eqir":
                 regs[dest] = arg1 == regs[arg2]
@@ -60,23 +74,45 @@ def run(pcr, prog, regs):
             case "eqrr":
                 regs[dest] = regs[arg1] == regs[arg2]
 
+            # new instructions
             case "modr":
-                # print(regs[arg1], regs[arg2], regs[arg1] % regs[arg2])
                 regs[dest] = regs[arg1] % regs[arg2]
+            case "divr":
+                regs[dest] = regs[arg1] // regs[arg2]
 
         pc = regs[pcr]
         pc += 1
 
-        # blah += 1
-        # if blah == 1000:
-        #     print(regs)
-        #     exit()
-
     return regs
+
+
+def refactor(prog):
+    # Get r2 mod r1
+    prog[2] = ("modr", 2, 1, 4)
+    prog[3] = ("eqri", 4, 0, 4)
+    prog[4] = ("addr", 4, 3, 3)
+    # if non-zero jump to finally
+    prog[5] = ("addi", 3, 5, 3)
+    # if zero add r1 to r0
+    prog[6] = ("addr", 1, 0, 0)
+    # ... and add co-factor
+    prog[7] = ("divr", 2, 1, 4)
+    prog[8] = ("addr", 4, 0, 0)
+    prog[9] = ("addi", 3, 1, 3)
+
+    # finally ...
+    prog[11] = ("addi", 1, 1, 1)
+    # Check if r1 * r1 > r2
+    prog[12] = ("mulr", 1, 1, 4)
+    prog[13] = ("gtrr", 4, 2, 4)
+    # ...
 
 
 def part1(data, args, p1_state):
     pcr, prog = data
+
+    refactor(prog)
+
     regs = boot()
     run(pcr, prog, regs)
     return regs[0]
@@ -85,11 +121,7 @@ def part1(data, args, p1_state):
 def part2(data, args, p1_state):
     pcr, prog = data
 
-    prog[2] = ("modr", 2, 1, 4)
-    prog[3] = ("gtri", 4, 0, 4)
-    prog[4] = ("addr", 4, 3, 3)
-    prog[5] = ("addr", 1, 0, 0)
-    prog[6] = ("addi", 3, 5, 3)
+    refactor(prog)
 
     regs = boot(1)
     run(pcr, prog, regs)
